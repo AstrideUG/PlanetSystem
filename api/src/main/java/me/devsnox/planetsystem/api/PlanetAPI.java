e26edf3527f3a9d44fbec4a378b625ff3ce81a69
DevSnox <yasin@dalal.ch>
2018-12-22 03:49:37 +0100

    Updated PlanetAPI.

--- api/src/main/java/me/devsnox/planetsystem/api/PlanetAPI.java
+++ api/src/main/java/me/devsnox/planetsystem/api/PlanetAPI.java
@@ -1,5 +1,15 @@
 package me.devsnox.planetsystem.api;
 
+import java.util.UUID;
+
 public interface PlanetAPI {
 
+    Planet getPlanet(UUID uuid);
+
+    /**
+     * Can only be used when the player is online
+     * @param uuid
+     * @return
+     */
+    PlanetPlayer getPlayer(UUID uuid);
 }
