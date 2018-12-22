18bbfc65276623d4a19bcfb3aba7183251370a83
DevSnox <yasin@dalal.ch>
2018-12-22 02:49:39 +0100

    Added PlayerListener.

---
+++ core/src/main/java/me/devsnox/planetsystem/core/listeners/PlayerListener.java
@@ -0,0 +1,18 @@
+package me.devsnox.planetsystem.core.listeners;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+
+public class PlayerListener implements Listener {
+
+
+    @EventHandler
+    public void onJoin() {
+
+    }
+
+    @EventHandler
+    public void onQuit() {
+
+    }
+}
