47d6f366b76abe2eb107562ab14d4b80676f1503
DevSnox <yasin@dalal.ch>
2018-12-22 04:05:52 +0100

    Updated PlanetSystem (registration of listeners and commands)

--- core/src/main/java/me/devsnox/planetsystem/core/PlanetSystem.java
+++ core/src/main/java/me/devsnox/planetsystem/core/PlanetSystem.java
@@ -1,6 +1,36 @@
 package me.devsnox.planetsystem.core;
 
+import me.devsnox.planetsystem.core.commands.PlanetCommand;
+import me.devsnox.planetsystem.core.listeners.PlanetListener;
+import me.devsnox.planetsystem.core.listeners.PlayerListener;
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.logging.Level;
+
 public class PlanetSystem extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        registerListeners();
+        registerCommands();
+
+        this.getLogger().log(Level.INFO, ""); //TODO: Add message
+    }
+
+    @Override
+    public void onDisable() {
+        this.getLogger().log(Level.INFO, ""); //TODO: Add message
+    }
+
+    private void registerCommands() {
+        this.getCommand("planet").setExecutor(new PlanetCommand());
+    }
+
+    private void registerListeners() {
+        PluginManager pluginManager = Bukkit.getPluginManager();
+        pluginManager.registerEvents(new PlanetListener(), this);
+        pluginManager.registerEvents(new PlayerListener(), this);
+    }
 }
