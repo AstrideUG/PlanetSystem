eb9321ea0cbef952e3f539fa160120a08c5ef583
DevSnox <yasin@dalal.ch>
2018-12-22 03:37:46 +0100

    Updated PlayerListener.

--- core/src/main/java/me/devsnox/planetsystem/core/listeners/PlayerListener.java
+++ core/src/main/java/me/devsnox/planetsystem/core/listeners/PlayerListener.java
@@ -1,18 +1,21 @@
 package me.devsnox.planetsystem.core.listeners;
 
+import me.devsnox.planetsystem.core.api.InternalPlanetFactory;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 
 public class PlayerListener implements Listener {
 
 
     @EventHandler
-    public void onJoin() {
-
+    public void onJoin(PlayerJoinEvent event) {
+        InternalPlanetFactory.internalPlanetAPI.loadPlayer(event.getPlayer().getUniqueId());
     }
 
     @EventHandler
-    public void onQuit() {
-
+    public void onQuit(PlayerQuitEvent event) {
+        InternalPlanetFactory.internalPlanetAPI.savePlayer(event.getPlayer().getUniqueId());
     }
 }
