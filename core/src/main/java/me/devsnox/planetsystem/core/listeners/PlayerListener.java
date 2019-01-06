1c51857479f94223acbfd994e0d2d887dda53f28
DevSnox <me.devsnox@gmail.com>
2019-01-06 10:54:30 +0100

    Fixed issue #19.

--- core/src/main/java/me/devsnox/planetsystem/core/listeners/PlayerListener.java
+++ core/src/main/java/me/devsnox/planetsystem/core/listeners/PlayerListener.java
@@ -1,9 +1,13 @@
 package me.devsnox.planetsystem.core.listeners;
 
+import me.devsnox.planetsystem.api.holder.Holder;
 import net.darkdevelopers.darkbedrock.darkness.spigot.events.PlayerDisconnectEvent;
+import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerRespawnEvent;
 
@@ -33,5 +37,10 @@ public class PlayerListener implements Listener {
     @EventHandler
     public void onRespawn(PlayerRespawnEvent event) {
         event.setRespawnLocation(holder.getPlanetData().getLoadedPlanetByOwner(event.getPlayer().getUniqueId()).getSpawnLocation().toBukkitLocation());
+    }
+
+    @EventHandler(priority = EventPriority.LOWEST)
+    public void onDamage(EntityDamageByEntityEvent event) {
+        if(event.getEntity().getType() == EntityType.PLAYER && event.getDamager().getType() == EntityType.PLAYER && Holder.Impl.holder.getWorld().equals(event.getEntity().getWorld())) event.setCancelled(true);
     }
 }
