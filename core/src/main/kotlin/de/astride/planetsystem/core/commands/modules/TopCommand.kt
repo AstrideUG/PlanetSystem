f04027c1529b858037f60eecedbfdb192802fd5e
LartyHD <lartyhd@gmail.com>
2019-03-04 14:25:06 +0100

    Impl "Planet Expand Top" Command #94

--- core/src/main/kotlin/de/astride/planetsystem/core/commands/modules/TopCommand.kt
+++ core/src/main/kotlin/de/astride/planetsystem/core/commands/modules/TopCommand.kt
@@ -1,7 +1,13 @@
 package de.astride.planetsystem.core.commands.modules
 
+import de.astride.planetsystem.api.holder.Holder
+import de.astride.planetsystem.api.planet.Planet
 import de.astride.planetsystem.api.player.PlanetPlayer
 import de.astride.planetsystem.core.commands.PlanetCommandModule
+import de.astride.planetsystem.core.functions.toPlanet
+import net.darkdevelopers.darkbedrock.darkness.general.minecraft.fetcher.Fetcher
+import net.darkdevelopers.darkbedrock.darkness.spigot.functions.sendTo
+import org.bukkit.ChatColor
 
 /**
  * @author Lars Artmann | LartyHD
@@ -10,8 +16,29 @@ import de.astride.planetsystem.core.commands.PlanetCommandModule
  */
 class TopCommand : PlanetCommandModule {
 
+    private val holder get() = Holder.instance
+
     override fun execute(planetPlayer: PlanetPlayer, args: Array<String>) {
-//        planetPlayer.sendConfigurableMessage("Planet.Command.TopCommand.Loading")
+        val prefix =
+            "${ChatColor.AQUA}${ChatColor.BOLD}Planets ${ChatColor.WHITE}┃${ChatColor.RESET} ${ChatColor.GREEN}"
+        "${prefix}Top #10".sendTo(planetPlayer.player)
+
+        @Suppress("UNCHECKED_CAST")
+        (holder.loadedPlanets as MutableSet<Planet>)
+            .apply {
+                holder.databaseHandler.allPlanets.map { it.toPlanet() }.forEach { planet ->
+                    if (none { it.owner == planet.owner }) this.add(planet)
+                }
+            }
+            .sortedBy { it.atmosphere.size }
+            .asReversed()
+            .take(10)
+            .withIndex()
+            .forEach { (index, planet) ->
+                "$prefix#${index + 1}: ${ChatColor.WHITE}${Fetcher.getName(planet.owner.uuid)} (${planet.atmosphere.size})"
+                    .sendTo(planetPlayer.player)
+            }
+
         planetPlayer.player.sendMessage("${usage.first().capitalize()} not implemented yet. Sorry :(")
     }
 
