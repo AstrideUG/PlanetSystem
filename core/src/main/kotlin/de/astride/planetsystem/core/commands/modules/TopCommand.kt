7c2b3567f74df8a94db7f889f16cc03fdbaa6149
LartyHD <lartyhd@gmail.com>
2019-03-12 08:39:14 +0100

    #104 review changes (use MessageKeys)

--- core/src/main/kotlin/de/astride/planetsystem/core/commands/modules/TopCommand.kt
+++ core/src/main/kotlin/de/astride/planetsystem/core/commands/modules/TopCommand.kt
@@ -4,9 +4,10 @@ import de.astride.planetsystem.api.holder.Holder
 import de.astride.planetsystem.api.planet.Planet
 import de.astride.planetsystem.api.player.PlanetPlayer
 import de.astride.planetsystem.core.commands.PlanetCommandModule
+import de.astride.planetsystem.core.functions.replace
 import de.astride.planetsystem.core.functions.toPlanet
+import de.astride.planetsystem.core.log.MessageKeys
 import net.darkdevelopers.darkbedrock.darkness.general.minecraft.fetcher.Fetcher
-import net.darkdevelopers.darkbedrock.darkness.spigot.functions.sendTo
 import org.bukkit.ChatColor
 
 /**
@@ -19,36 +20,37 @@ class TopCommand : PlanetCommandModule {
     private val holder get() = Holder.instance
 
     override fun execute(planetPlayer: PlanetPlayer, args: Array<String>) {
-        val prefix =
-            "${ChatColor.AQUA}${ChatColor.BOLD}Planets ${ChatColor.WHITE}┃${ChatColor.RESET} ${ChatColor.GREEN}"
 
-        val player = planetPlayer.player
-        if (args.size > 1) {
-            //TODO: Send better error message (use a better message system)
-            "Use /planet Top [size]".sendTo(player)
-            return
-        }
+        if (args.size > 1) planetPlayer.logger.warn(MessageKeys.COMMANDS_TOP_FAILED_ARGS_SIZE_BIGGER_1)
+        else {
 
-        val size = args.ifEmpty { arrayOf(10) }[0].toString().toIntOrNull() ?: 10
+            val size = args.ifEmpty { arrayOf(10) }[0].toString().toIntOrNull() ?: 10
 
+            planetPlayer.logger.info(
+                MessageKeys.COMMANDS_TOP_SUCCESSES_SIZE_INFO.toString().replace("<size>", size.toString(), true)
+            )
 
-        "${prefix}Top #$size".sendTo(player)
-
-        @Suppress("UNCHECKED_CAST")
-        (holder.loadedPlanets.toMutableSet() as MutableSet<Planet>)
-            .apply {
-                holder.databaseHandler.allPlanets.map { it.toPlanet() }.forEach { planet ->
-                    if (none { it.owner == planet.owner }) this.add(planet)
+            @Suppress("UNCHECKED_CAST")
+            (holder.loadedPlanets.toMutableSet() as MutableSet<Planet>)
+                .apply {
+                    holder.databaseHandler.allPlanets.map { it.toPlanet() }.forEach { planet ->
+                        if (none { it.owner == planet.owner }) this.add(planet)
+                    }
                 }
-            }
-            .sortedBy { it.atmosphere.size }
-            .take(size)
-            .asReversed()
-            .withIndex()
-            .forEach { (index, planet) ->
-                "$prefix#${index + 1}: ${ChatColor.WHITE}${Fetcher.getName(planet.owner.uuid)} (${planet.atmosphere.size})"
-                    .sendTo(player)
-            }
+                .sortedBy { it.atmosphere.size }
+                .take(size)
+                .asReversed()
+                .withIndex()
+                .forEach { (index, planet) ->
+
+                    planetPlayer.logger.info(
+                        MessageKeys.COMMANDS_TOP_SUCCESSES_SIZE_INFO.toString()
+                            .replace("<place>", (index + 1).toString(), true)
+                            .replace("<name>", Fetcher.getName(planet.owner.uuid) ?: "${ChatColor.ITALIC}Unknown", true)
+                            .replace(atmosphere = planet.atmosphere)!!
+                    )
+                }
+        }
 
     }
 
