e2910306fb1b5e1d21cdcc973da8bad054a01a28
LartyHD <lartyhd@gmail.com>
2019-03-12 09:32:28 +0100

    #119 review changes (added configuration to Flags)

--- core/src/main/kotlin/de/astride/planetsystem/core/flags/Flags.kt
+++ core/src/main/kotlin/de/astride/planetsystem/core/flags/Flags.kt
@@ -1,14 +1,19 @@
 package de.astride.planetsystem.core.flags
 
+import com.google.gson.JsonArray
+import de.astride.planetsystem.core.service.ConfigService
 import org.bukkit.World
 import org.bukkit.entity.EntityType
+import org.bukkit.entity.EntityType.*
 
 /**
  * @author Lars Artmann | LartyHD
  * Created by Lars Artmann | LartyHD on 04.03.2019 06:03.
- * Current Version: 1.0 (04.03.2019 - 04.03.2019)
+ * Current Version: 1.0 (04.03.2019 - 12.03.2019)
  */
 sealed class Flags {
+
+    protected val flags get() = ConfigService.instance.flags
 
     abstract var value: Boolean
 
@@ -17,55 +22,57 @@ sealed class Flags {
     }
 
     object Animals : Flags() {
-        override var value: Boolean = true
 
-        val types = arrayOf(
-            EntityType.WOLF,
-            EntityType.PIG,
-            EntityType.SHEEP,
-            EntityType.COW,
-            EntityType.CHICKEN,
-            EntityType.SQUID,
-            EntityType.BAT,
-            EntityType.MUSHROOM_COW,
-//            SNOWMAN,
-            EntityType.OCELOT,
-            EntityType.HORSE,
-            EntityType.RABBIT,
-            EntityType.VILLAGER
+        override var value: Boolean = flags.animals.value
+
+        val types = flags.animals.types.convertTo(
+            WOLF,
+            PIG,
+            SHEEP,
+            COW,
+            CHICKEN,
+            SQUID,
+            BAT,
+            MUSHROOM_COW,/* SNOWMAN, */
+            OCELOT,
+            HORSE,
+            RABBIT,
+            VILLAGER
         )
 
     }
 
     object Mobs : Flags() {
-        override var value: Boolean = true
 
-        val types = arrayOf(
-            EntityType.IRON_GOLEM,
-            EntityType.CREEPER,
-            EntityType.SKELETON,
-            EntityType.SPIDER,
-            EntityType.GIANT,
-            EntityType.ZOMBIE,
-            EntityType.SLIME,
-            EntityType.GHAST,
-            EntityType.PIG_ZOMBIE,
-            EntityType.ENDERMAN,
-            EntityType.CAVE_SPIDER,
-            EntityType.SILVERFISH,
-            EntityType.BLAZE,
-            EntityType.MAGMA_CUBE,
-            EntityType.ENDER_DRAGON,
-            EntityType.WITHER,
-            EntityType.WITCH,
-            EntityType.ENDERMITE,
-            EntityType.GUARDIAN
+        override var value: Boolean = flags.mobs.value
+
+        val types = flags.mobs.types.convertTo(
+            IRON_GOLEM,
+            CREEPER,
+            SKELETON,
+            SPIDER,
+            GIANT,
+            ZOMBIE,
+            SLIME,
+            GHAST,
+            PIG_ZOMBIE,
+            ENDERMAN,
+            CAVE_SPIDER,
+            SILVERFISH,
+            BLAZE,
+            MAGMA_CUBE,
+            ENDER_DRAGON,
+            WITHER,
+            WITCH,
+            ENDERMITE,
+            GUARDIAN
         )
+
     }
 
     object FireTick : Flags() {
 
-        var world: World? = null
+        var world: World? = flags.fireTick.world
 
         override var value: Boolean = false
             set(value) {
@@ -74,13 +81,17 @@ sealed class Flags {
             }
 
         init {
-            value = false
+            value = flags.fireTick.value
         }
 
     }
 
     object PvP : Flags() {
-        override var value: Boolean = false
+        override var value: Boolean = flags.pvp.value
     }
 
 }
+
+
+private fun JsonArray?.convertTo(vararg default: EntityType) =
+    this?.map { valueOf(it.asString.toUpperCase()) }?.toSet() ?: setOf(*default)
