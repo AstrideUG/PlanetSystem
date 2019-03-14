e024104c165d4da29b22b408ac095a500233e006
LartyHD <lartyhd@gmail.com>
2019-03-14 12:02:03 +0100

    Save the Atmosphere of the Planet in Database #83

--- api/src/main/kotlin/de/astride/planetsystem/api/database/DatabasePlanet.kt
+++ api/src/main/kotlin/de/astride/planetsystem/api/database/DatabasePlanet.kt
@@ -1,15 +1,17 @@
 package de.astride.planetsystem.api.database
 
+import de.astride.planetsystem.api.atmosphere.Atmosphere
+import de.astride.planetsystem.api.inline.Owner
+import de.astride.planetsystem.api.inline.UniqueID
 import de.astride.planetsystem.api.location.PlanetLocation
-import java.util.*
 
 interface DatabasePlanet {
 
-    val uuid: UUID
+    val uuid: UniqueID
     val name: String
-    val ownerUniqueId: UUID
-    val members: Set<UUID>
-    val size: Byte
+    val ownerUniqueId: Owner
+    val members: Set<Owner>
+    val atmosphere: Atmosphere
     val planetLocation: PlanetLocation
     val metaData: Map<String, Any>
 
