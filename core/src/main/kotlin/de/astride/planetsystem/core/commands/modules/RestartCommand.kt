5cc178e57b6c9fb5ec84ae78a1144596f117979f
LartyHD <lartyhd@gmail.com>
2019-06-11 00:05:27 +0200

    Fixed a restart command message

--- core/src/main/kotlin/de/astride/planetsystem/core/commands/modules/RestartCommand.kt
+++ core/src/main/kotlin/de/astride/planetsystem/core/commands/modules/RestartCommand.kt
@@ -1,8 +1,8 @@
 package de.astride.planetsystem.core.commands.modules
 
-import de.astride.planetsystem.api.holder.Holder
 import de.astride.planetsystem.api.player.PlanetPlayer
 import de.astride.planetsystem.api.player.isOnHisPlanet
+import de.astride.planetsystem.api.proxies.databaseHandler
 import de.astride.planetsystem.core.commands.PlanetCommandModule
 import de.astride.planetsystem.core.database.entities.BasicDatabasePlanet
 import de.astride.planetsystem.core.log.MessageKeys
@@ -12,24 +12,26 @@ import net.darkdevelopers.darkbedrock.darkness.spigot.functions.messages
 /**
  * @author Lars Artmann | LartyHD
  * Created by Lars Artmann | LartyHD on 14.03.2019 14:11.
- * Current Version: 1.0 (14.03.2019 - 18.03.2019)
+ * Current Version: 1.0 (14.03.2019 - 10.06.2019)
  */
 class RestartCommand : PlanetCommandModule {
 
-    /**
-     * @author Lars Artmann | LartyHD
-     * Created by Lars Artmann | LartyHD on 14.03.2019 14:11.
-     * Current Version: 1.0 (14.03.2019 - 18.03.2019)
-     */
-    override fun execute(planetPlayer: PlanetPlayer, args: Array<String>) = if (planetPlayer.isOnHisPlanet()) {
+    override fun execute(planetPlayer: PlanetPlayer, args: Array<String>) {
+        if (!planetPlayer.isOnHisPlanet()) {
+            planetPlayer.logger.warn(MessageKeys.COMMANDS_RESTART_FAILED_NOT_OWN_PLANET)
+            return
+        }
         when {
-            args.isEmpty() -> planetPlayer.player.openInventory(ConfigService.instance.config.commands.restart.inventory)
+            args.isEmpty() -> {
+                planetPlayer.player.openInventory(ConfigService.instance.config.commands.restart.inventory)
+                planetPlayer.logger.success(MessageKeys.COMMANDS_RESTART_SUCCESS)
+            }
             args.size == 1 && "confirmed".equals(args[0], true) -> {
 
                 planetPlayer.player.kickPlayer(
                     messages["Planet.Command.Planet.Restart.confirmed.kick"] ?: "Planet deleted"
                 )
-                Holder.instance.databaseHandler.deletePlanet(BasicDatabasePlanet.by(planetPlayer.planet))
+                databaseHandler.deletePlanet(BasicDatabasePlanet.by(planetPlayer.planet))
                 //Don't delete the schematic
 
             }
@@ -37,8 +39,6 @@ class RestartCommand : PlanetCommandModule {
                 /* TODO Send usage */
             }
         }
-
-        planetPlayer.logger.success(MessageKeys.COMMANDS_INFO_SUCCESS)
-    } else planetPlayer.logger.warn(MessageKeys.COMMANDS_RESTART_FAILED_NOT_OWN_PLANET)
+    }
 
 }
